bf0885739f0a1a1fa8acf4480ff1d9d723ad4e7a
BryanWieschenberg <bryan.wieschenberg@gmail.com>
2025-07-02 21:04:41 -0400

    a

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
+    console.log("🔒 Middleware triggered");
     const authHeader = req.headers.get("authorization");
 
     if (authHeader) {
         const [scheme, encoded] = authHeader.split(" ");
         if (scheme === "Basic") {
             const [_username, password] = atob(encoded).split(":");
+            console.log("🔐 Password received:", password);
             if (password === process.env.DEPLOY_PASS) {
                 return NextResponse.next();
             }
         }
     }
 
+    console.warn("❌ Unauthorized access attempt");
     return new Response("Unauthorized", {
         status: 401,
         headers: {
